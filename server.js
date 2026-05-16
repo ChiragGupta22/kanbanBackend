0c8069c8e9022bea546666b6d854d5fe5ce60ad0
Chirag <chiragguptaworks@gmail.com>
2026-05-16 12:07:17 +0530

    changes to server.js

--- server.js
+++ server.js
@@ -1,34 +1,8 @@
 import { config } from "dotenv";
 import { connectDB } from "./src/db/db.js";
 import app from "./src/app.js";
-import http from "http";
-import { Server } from "socket.io";
 
 config();
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173", "https://kanban-frontend-s.vercel.app"],
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("joinRoom", ({ projectId }) => {
-    socket.join(projectId);
-
-    console.log(` joined ${projectId} `);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
 
 server.listen(5000, async () => {
   await connectDB();
