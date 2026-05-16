b481b42a086558551d2f576e5720f55f46020ee1
Chirag <chiragguptaworks@gmail.com>
2026-05-16 12:30:53 +0530

    changes to app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://kanban-frontend-s.vercel.app",
+    origin: "https://kanban-frontend-tan.vercel.app",
     credentials: true,
   }),
 );
