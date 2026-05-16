feb6bf2a487a1f35fe4cc3291c647650268c7848
Chirag <chiragguptaworks@gmail.com>
2026-05-16 12:27:49 +0530

    changes to usercontroller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -82,7 +82,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(201).json({
